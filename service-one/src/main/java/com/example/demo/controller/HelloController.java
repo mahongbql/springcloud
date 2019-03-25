25f6d0507c64f7bd202f3b759cdb91fd8c68545c
mahongbin <mahongbin@youxin.com>
2019-03-25 17:04:29 +0800

    配置中心客户端

--- service-one/src/main/java/com/example/demo/controller/HelloController.java
+++ service-one/src/main/java/com/example/demo/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,13 @@ public class HelloController {
 
     private Logger log = LoggerFactory.getLogger(HelloController.class);
 
+    @Value("${config}")
+    private String config;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         log.info("service-one accept ------------------------------ ");
 
-        return "hello world";
+        return this.config;
     }
 }
