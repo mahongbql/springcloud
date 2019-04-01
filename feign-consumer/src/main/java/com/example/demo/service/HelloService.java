1053e4057a1c8cdb7c029ad6cbfb1105bc22bb17
mahongbin <mahongbin@youxin.com>
2019-04-01 16:33:03 +0800

    feign

---
+++ feign-consumer/src/main/java/com/example/demo/service/HelloService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@FeignClient("hello-service")
+public interface HelloService {
+
+    @RequestMapping("hello")
+    String hello();
+}
