0e012bf50ecc3c636d33bd975ffeda07bca8b329
mahongbin <mahongbin@youxin.com>
2019-04-28 17:19:57 +0800

    netty

---
+++ netty/src/main/java/com/example/netty/EchoClientHandler.java
@@ -0,0 +1,46 @@
+package com.example.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+import java.nio.charset.Charset;
+
+/**
+ * @author mahongbin
+ * @date 2019/4/28 13:37
+ * @Description netty客户端
+ */
+@ChannelHandler.Sharable
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("client channelActive..");
+        // 必须有flush
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    /**
+     * 该方法执行完成之后，SimpleChannelInboundHandler 会释放指向保存该消息的 ByteBuf 的内存引用
+     * @param channelHandlerContext
+     * @param byteBuf
+     * @throws Exception
+     */
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+        System.out.println("client channelRead..");
+        ByteBuf buf = byteBuf.readBytes(byteBuf.readableBytes());
+        System.out.println("Client received:" + ByteBufUtil.hexDump(buf) + "; The value is:" + buf.toString(Charset.forName("utf-8")));
+    }
+}
