d7dd7d833def74a03d1e1c3008246f3b090d2f51
mahongbin <mahongbin@youxin.com>
2019-05-07 18:13:19 +0800

    netty

---
+++ netty/src/main/java/webSocket/NioWebSocketChannelInitializer.java
@@ -0,0 +1,30 @@
+package webSocket;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+
+/**
+ * @author mahongbin
+ * @date 2019/5/7 15:18
+ * @Description
+ */
+public class NioWebSocketChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        //设置log监听器，并且日志级别为debug，方便观察运行流程
+        ch.pipeline().addLast("logging",new LoggingHandler("DEBUG"));
+        //设置解码器
+        ch.pipeline().addLast("http-codec",new HttpServerCodec());
+        //聚合器，使用websocket会用到
+        ch.pipeline().addLast("aggregator",new HttpObjectAggregator(65536));
+        //用于大数据的分区传输
+        ch.pipeline().addLast("http-chunked",new ChunkedWriteHandler());
+        //自定义的业务handler
+        ch.pipeline().addLast("handler",new NioWebSocketHandler());
+    }
+}
